5b5c254e0f811fd35c959ef8fb5d556b3f3545cd
SWIFTKENKA <hunterghost526@gmail.com>
2020-10-05 07:59:12 -0400

    Configured resources. Added main page example

--- src/main/java/edu/inter/uiprrasdddl/config/WebConfig.java
+++ src/main/java/edu/inter/uiprrasdddl/config/WebConfig.java
@@ -3,7 +3,9 @@ package edu.inter.uiprrasdddl.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 @EnableWebMvc
@@ -11,5 +13,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         "edu.inter.uiprrasdddl.controller"
 })
 public class WebConfig implements WebMvcConfigurer {
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/"
+    };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
 
 }
