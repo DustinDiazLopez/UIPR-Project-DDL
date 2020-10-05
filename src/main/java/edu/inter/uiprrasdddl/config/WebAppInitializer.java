37729b73f95a214034684977a6b7feeb4a095710
SWIFTKENKA <hunterghost526@gmail.com>
2020-10-05 12:16:08 -0400

    refactored - redirects to actual page

--- src/main/java/edu/inter/uiprrasdddl/config/WebAppInitializer.java
+++ src/main/java/edu/inter/uiprrasdddl/config/WebAppInitializer.java
@@ -1,27 +1,27 @@
-package edu.inter.uiprrasdddl.config;
-
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[] {
-                AppConfig.class
-        };
-    }
-
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class[] {
-                WebConfig.class
-        };
-    }
-
-    @Override
-    protected String[] getServletMappings() {
-        return new String[] {
-                "/"
-        };
-    }
-}
+//package edu.inter.uiprrasdddl.config;
+//
+//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+//
+//public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+//
+//    @Override
+//    protected Class<?>[] getRootConfigClasses() {
+//        return new Class[] {
+//                AppConfig.class
+//        };
+//    }
+//
+//    @Override
+//    protected Class<?>[] getServletConfigClasses() {
+//        return new Class[] {
+//                WebConfig.class
+//        };
+//    }
+//
+//    @Override
+//    protected String[] getServletMappings() {
+//        return new String[] {
+//                "/"
+//        };
+//    }
+//}
--- src/main/java/edu/inter/uiprrasdddl/config/WebConfig.java
+++ src/main/java/edu/inter/uiprrasdddl/config/WebConfig.java
@@ -20,8 +20,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
-
 }
