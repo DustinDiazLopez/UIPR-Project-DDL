3e71f8045289ddda72d52870fac6cb569798abc2
SWIFTKENKA <hunterghost526@gmail.com>
2020-11-11 22:38:32 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once('consts.php');
-echo '<h1>Go to: <a target="_blank" href="'.DEV_SITE.'">Development Demo Site</a></h1>';
+//echo '<h1>Go to: <a target="_blank" href="'.DEV_SITE.'">Development Demo Site</a></h1>';
+echo '<h1 style="color:red;">Demo Site: <a target="_blank" href="#" value="'.DEV_SITE.'">Down For Maintenance</a></h1>';
 echoPaths('Site is under construction');
 
