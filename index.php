f33bc9ecacf6d067b0d282c9ddbb9316fbadc3b1
SWIFTKENKA <hunterghost526@gmail.com>
2020-11-16 18:23:11 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,68 +1,6 @@
-<style>
-:root {
-    --green: #008066;
-    --yellow: #FED557;
-}
-
-.cap {
-    text-transform: capitalize;
-}
-
-a:link {
-    text-decoration: none;
-    color: var(--green) !important;
-}
-
-a:visited {
-    text-decoration: none;
-}
-
-a:hover {
-    text-decoration: underline;
-}
-
-a:active {
-    text-decoration: underline;
-}
-
-
-
-:target {
-    border-radius: 3px;
-    animation: highlight 1000ms ease-out;
-}
-
-@keyframes highlight {
-    0% {
-        background-color: var(--green);
-    }
-
-    100% {
-        background-color: inherit;
-    }
-}
-
-.highlight{
-    color: var(--green);
-    background-color: var(--yellow);
-}
-
-
-::selection {
-    background: var(--yellow);
-    color: var(--green);
-}
-
-::-moz-selection {
-    background: var(--yellow);
-    color: var(--green);
-}
-</style>
-
-
 <?php
 include_once('consts.php');
 echo '<h1>Demo Site: <a target="_blank" href="'.DEV_SITE.'">Go</a></h1>';
 //echo '<h1>Demo Site: <span style="color:red">Down for Maintenance</span></h1>';
-echoPaths('<h2>Testing In Progress (II)</h2>');
+echoPaths('<h2>Site is no longer under development:</h2>');
 
