15425a637d0819a43d375bf2aba73081a5b553cc
SWIFTKENKA <hunterghost526@gmail.com>
2020-11-15 23:27:03 -0400

    Update releases.php

--- redir/releases.php
+++ redir/releases.php
@@ -2,7 +2,7 @@
 include_once('../consts.php');
 
 if (isset($_GET['release']) && !empty($_GET['release']) ) {
-    redir_release(urldecode($_GET['release']);
+    redir_release(urldecode($_GET['release']));
 } else {
     redir_releases();
 }
