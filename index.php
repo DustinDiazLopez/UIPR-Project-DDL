b4dbd9e62c8cde8dceb38c1a0ade65d63327dcd3
SWIFTKENKA <hunterghost526@gmail.com>
2020-11-23 02:31:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 include_once('consts.php');
 echo '<h1>Final Demo Site: <a target="_blank" href="'.DEV_SITE.'">Visit</a></h1>';
+echo "<pre>username: colon\npassword: hello-password</pre><br />";
 //echo '<h1>Demo Site: <span style="color:red">Down for Maintenance</span></h1>';
-echoPaths('<h2>Site is no longer under development:</h2>');
+echoPaths('<h2>Site is no longer under development (11/16/2020):</h2>');
 
