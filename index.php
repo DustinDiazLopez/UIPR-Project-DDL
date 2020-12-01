b89fc60f1a0bc51dd331667bcd02b67fa455fc54
DustinDiazLopez <hunterghost526@gmail.com>
2020-12-01 09:47:22 -0400

    redir

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 include_once('consts.php');
-echo '<h1>Final Demo Site: <a target="_blank" href="'.DEV_SITE.'">Visit</a></h1>';
+echo '<h1>Site: <a target="_blank" href="'.DEV_SITE.'">Visit</a></h1>';
 echo "<pre>username: colon\npassword: hello-password</pre><br />";
 //echo '<h1>Demo Site: <span style="color:red">Down for Maintenance</span></h1>';
 echoPaths();
-
