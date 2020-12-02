6f97fb1e1955a098640a3eae22d6e68375b45ac1
DustinDiazLopez <hunterghost526@gmail.com>
2020-12-02 14:19:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 include_once('consts.php');
 echo '<h1>Site: <a target="_blank" href="'.DEV_SITE.'">Visit</a></h1>';
-echo "<pre>username: colon\npassword: hello-password</pre><br />";
-//echo '<h1>Demo Site: <span style="color:red">Down for Maintenance</span></h1>';
+//echo '<h1>Demo Site: <span style="color:red">Down</span></h1>';
 echoPaths();
